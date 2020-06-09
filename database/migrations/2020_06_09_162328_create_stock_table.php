6a7443bbd403a0784989f04739870ad9ab9700d4
kabae-iscteiulpt <kabae@iscte-iul.pt>
2020-06-09 17:58:42 +0100

    i create a project BetOnStockMarket with a laravel

---
+++ database/migrations/2020_06_09_162328_create_stock_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('name');
+            $table->String('symbol');
+            $table->double('stockvaluebefore');
+            $table->double('currentstockvalue');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
