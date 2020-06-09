da19ebc9c04c964cf0a41218dfe45c3d6065c21a
kabae-iscteiulpt <kabae@iscte-iul.pt>
2020-06-09 18:29:09 +0100

    some adjustment to make our first page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,6 +60,7 @@
 
             .m-b-md {
                 margin-bottom: 30px;
+                border-style: solid ;
             }
         </style>
     </head>
@@ -80,19 +81,12 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div id="imgOfPig">
+                    <img src="assets/cofrinho.svg" alt="cofrinho" width="300" height="300">
+                </div>
+                <div id="betstockmarket"class="title m-b-md">
+                    Bet On Stock Market
                 </div>
             </div>
         </div>
